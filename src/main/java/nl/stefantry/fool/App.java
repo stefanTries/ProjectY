8392424a166cc2a5856383baa1d461285003c994
Stefan <INFO@BUDGETSLOTS.COM>
2014-12-17 23:50:29 +0100

    messitsj

---
+++ src/main/java/nl/stefantry/fool/App.java
@@ -0,0 +1,13 @@
+package nl.stefantry.fool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
